12d4fd508d8ea0288a45ef151a0170d600d828a9
Vivekanandan Ramalingam <r_vivekit@yahoo.co.in>
2015-09-15 15:59:19 -0400

    SimplifiedPO

---
+++ SJ/SimplifiedPO/SimplifiedPO/SimplifiedPOBusiness/SPOBL.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+using SimplifiedPODataAccess;
+
+namespace SimplifiedPOBusiness
+{
+    public class SPOBL
+    {
+        private readonly Logger _nlog = LogManager.GetCurrentClassLogger();
+        private SimplifiedPODataAccess.SPODL _access = new SPODL();
+        public SPOBL()
+        {
+            _nlog.Trace(message:
+                this.GetType().Namespace + ":" + MethodBase.GetCurrentMethod().DeclaringType.Name + ":" +
+                System.Reflection.MethodBase.GetCurrentMethod().Name + "::Entering");
+            _access = new SPODL();
+            _nlog.Trace(message:
+                this.GetType().Namespace + ":" + MethodBase.GetCurrentMethod().DeclaringType.Name + ":" +
+                System.Reflection.MethodBase.GetCurrentMethod().Name + "::Exit");
+        }
+
+        public DataTable SalesTransationsGroupedByItem(DateTime startdate, DateTime enddate, int strNbr)
+        {
+            _nlog.Trace(message:
+                this.GetType().Namespace + ":" + MethodBase.GetCurrentMethod().DeclaringType.Name + ":" +
+                System.Reflection.MethodBase.GetCurrentMethod().Name + "::Entering");
+            return _access.Test(startdate, enddate, strNbr);
+
+        }
+
+        public string TestMethod(string testString)
+        {
+            _nlog.Trace(message:
+    this.GetType().Namespace + ":" + MethodBase.GetCurrentMethod().DeclaringType.Name + ":" +
+    System.Reflection.MethodBase.GetCurrentMethod().Name + "::Entering");
+            return _access.Test(testString);
+        }
+
+
+    }
+}
---
+++ SJ/SimplifiedPO/SimplifiedPO/SimplifiedPOConstants/SPOConst.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplifiedPOConstants
+{
+    public class SPOConst
+    {
+        public static string DefaultString = ConfigurationManager.ConnectionStrings["EPMConnection"].ConnectionString;
+        public string DefaultConnectionString
+        {
+            get
+            {
+                return DefaultString;
+            }
+            set
+            {
+                DefaultString = value;
+            }
+        }
+
+        public string SalesTransationsGroupedByItem = "SalesTransationsGroupedByItem";
+        public string StoreSalesGroupedByItem = "[EPM].[EPM_DW].[dbo].[SalesTransationsGroupedByItem]";
+        public string CostcoSalesReport = "CostcoSalesMvmt";
+    }
+}
